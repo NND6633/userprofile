e4a085ff16a6c49222c8ef0201af05437c47e10f
Your Name <you@example.com>
2025-02-10 21:09:59 +0700

    HI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+// vite.config.js
+export default {
+  server: {
+    port: process.env.PORT || 3000, // Cổng mặc định là 3000 hoặc sử dụng cổng được Render cung cấp
+  },
+};
